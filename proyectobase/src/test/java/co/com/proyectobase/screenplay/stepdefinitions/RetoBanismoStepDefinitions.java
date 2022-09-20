3593b9081877ac9247715209506413199a5aa8ec
Carlos Alberto Lopez Muriel <carlos6171@gmail.com>
2022-09-19 21:43:26 -0500

    Se crea el feature y las clases Runner y StepDefinitions

---
+++ proyectobase/src/test/java/co/com/proyectobase/screenplay/stepdefinitions/RetoBanismoStepDefinitions.java
@@ -0,0 +1,35 @@
+package co.com.proyectobase.screenplay.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RetoBanismoStepDefinitions {
+
+    @Given("^Deseo ingresar a la pagina web de Banismo$")
+    public void deseoIngresarALaPaginaWebDeBanismo() {
+
+    }
+
+
+    @When("^ingreso al menu Acerca de nosotros$")
+    public void ingresoAlMenuAcercaDeNosotros() {
+
+    }
+
+    @When("^selecciono la opcion Informacion Corporativa doy click en la opcion Sostenibilidad$")
+    public void seleccionoLaOpcionInformacionCorporativaDoyClickEnLaOpcionSostenibilidad() {
+
+    }
+
+    @When("^selecciono de la lista la opcion Generacion sostenible, descargo la edicion # (\\d+) del boletin en formato pdf$")
+    public void seleccionoDeLaListaLaOpcionGeneracionSostenibleDescargoLaEdicionDelBoletinEnFormatoPdf(int arg1) {
+
+    }
+
+    @Then("^verifico que el PDF generado sea el correcto\\.$")
+    public void verificoQueElPDFGeneradoSeaElCorrecto() {
+
+    }
+
+}
---
+++ proyectobase/src/test/resources/features/Reto_Banismo.feature
@@ -0,0 +1,12 @@
+#Autor: Carlos Alberto Lopez Muriel
+
+Feature: Automatizacion pagina web Banismo
+  como usuario quiero ingresar a la pagina web de Banismo para ingresar a la seccion de sostenibilidad
+  y poder descargar la edicion # 9 del boletin Generacion sostenible en formato pdf.
+
+  Scenario: Descarga exitosa boletin Generacion sostenible edicion # 9
+    Given Deseo ingresar a la pagina web de Banismo
+    When ingreso al menu Acerca de nosotros
+    When selecciono la opcion Informacion Corporativa doy click en la opcion Sostenibilidad
+    When  selecciono de la lista la opcion Generacion sostenible, descargo la edicion # 9 del boletin en formato pdf
+    Then verifico que el PDF generado sea el correcto.
